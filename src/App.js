d18f09a24547927c0e3aaa5952d151fa1e3cf681
“irwinj7” <Irwinjorge@pursuit.com>
2023-04-23 13:10:25 -0400

    finished lab

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import React from "react";
 import "./App.css";
+import { useState } from "react";
 
 function App () {
+    const [score, setScore] = useState(0)
+    const [pointIncrement, setPointIncrement] = useState(1)
+
+    function increaseScore(){
+      setScore(score + pointIncrement)
+    }
+
+    function increasePointIncrement(){
+      if (score >= 10) {
+        setScore(score - 10)
+        setPointIncrement(pointIncrement + 1)
+      } else {
+        window.alert("You can't afford that!")
+      }
+    }
+
+    function resetScore(){
+      setScore(0)
+      setPointIncrement(1)
+    }
     return (
       <main>
-        <p>React State Lab</p>
+        {score < 100 ? (
+          <div>
+            <h1>Current Score: {score}</h1>
+            <button onClick={increaseScore}>+{pointIncrement}</button>
+            <button onClick={increasePointIncrement}>Pay 10 points to change from +{pointIncrement} to +{pointIncrement + 1}</button>
+          </div>
+        ) : (
+          <div>
+          <h2>You Win!</h2>
+          <button onClick={resetScore}>Play again?</button>
+          </div>
+        )}
       </main>
     );
 }
+
 
 export default App;
