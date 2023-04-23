ecb4953f9ce2469479a4644382061c90f7526c75
Phillip Mitchell <4kphil.@Phillips-MacBook-Air.local>
2023-04-23 14:04:14 -0400

    I think I'm done lol

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from "react";
-import "./App.css";
 
-function App () {
-    return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
+import "./App.css";
+import { React, useState } from "react";
+import Game from "./Game.js";
+import Restart from "./Restart.js";
+
+function App() {
+  const [win, setWin] = useState(false);
+  const [score, setScore] = useState(0);
+  const [inc, setInc] = useState(1);
+  return (
+    <main>
+      <p>React State Lab</p>
+      {!win ? (
+        <Game
+          setWin={setWin}
+          win={win}
+          setInc={setInc}
+          setScore={setScore}
+          score={score}
+          inc={inc}
+        />
+      ) : null}
+      {win ? (
+        <Restart
+          setWin={setWin}
+          win={win}
+          setInc={setInc}
+          setScore={setScore}
+        />
+      ) : null}
+    </main>
+  );
 }
 
 export default App;
