e38dbb45a300a82259bce280366554ad380c383f
JenniferEinstein <jennifereinstein@gmail.com>
2023-04-23 12:46:02 -0400

    Initial commit. Class coding through function buttonScore.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React from "react";
 import "./App.css";
+import { useState } from "react";
 
 function App () {
+  const [score, setScore] = useState(0)
+  const [buttonNumber, setButtonNumber] = useState(1)
+
+  function increaseScore() {
+    setScore(score + buttonNumber)
+  }
+
+  function buttonScore() {
+    if(score<10){
+      window.alert("You can't afford that!")
+    } else {
+    setScore(score-10)
+    setButtonNumber(buttonNumber+1)
+  }}
+
+  
     return (
-      <main>
-        <p>React State Lab</p>
-      </main>
+      <>
+        <main>
+          <p>React State Lab</p>
+          <h1>Current Score: {score}</h1>
+          <button onClick={increaseScore}>+1</button>
+          <button onClick={buttonScore}>Pay 10 points to change from +1 to +2</button>
+        </main>
+      </>
     );
+    
 }
 
 export default App;
+
+/* ------- psuedocode for this lab -------
+- My score starts at 0. The goal is to get to 100. (state: score)
+- I want a button that will increase my score by the number of points on said button. This starts at 1. (addButton)
+- I want a button that gives me the option to increase the number of the addButton. Using the button will decrease my score by ten. (decrementButton)
+- If I try to use the decrement button without having enough points, I should get a window popping up saying that I cannot do that (window)
+- When I reach 100, I should get a messsage saying that I WIN! and there should be an option to restart the game. (message, restartButton)
+
+*/
