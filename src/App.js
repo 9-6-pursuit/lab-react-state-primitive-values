f733380a4d41642ac7786ab0c77b058b9e7876a2
antunishdPursuit <dennysantunish@pursuit.org>
2023-04-23 12:59:37 -0400

    Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App () {
-    return (
+  const [score, setScore] = useState(0)
+  const [payment, setPayment] = useState(1)
+
+  function increaseScore(){
+    setScore(score + payment)
+  }
+
+  function increasePayment(){
+    if (score >= 10) {
+    setScore(score - 10)
+    setPayment(payment + 1)
+    } else {
+      window.alert("You can't afford that!")
+    }
+  }
+
+  function resetScore(){
+    setScore(0)
+    setPayment(1)
+  }
+  return (
       <main>
-        <p>React State Lab</p>
+        {score < 100 ? (<div>
+          <h1>Current Score: {score}</h1>
+          <button onClick={increaseScore}>+{payment}</button>
+          <button onClick={increasePayment}>Pay 10 points to change from +{payment} to +{payment + 1}</button>
+        </div>) : (<div>
+          <h2>You Win!</h2>
+        <button onClick={resetScore}>Play again?</button>
+        </div>)}
+
       </main>
     );
 }
