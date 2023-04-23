02495d8f7c6b375d0a7fa1db631ff6ed1a07d263
RK-404 <rizwan_k@RIZWANs-MacBook-Air.local>
2023-04-23 19:44:39 -0400

    Solved Lab

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App () {
-    return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
+  const [score, setScore] = useState(0);
+  const [pointIncrement, setPointIncrement] = useState(1);
+
+  function increaseScore() {
+    setScore(score + pointIncrement);
+  }
+  function startOver() {
+    setScore(0);
+    setPointIncrement(1);
+  }
+
+  function increasePointIncrement() {
+    if (score >= 10) {
+      setScore(score - 10);
+      setPointIncrement(pointIncrement + 1);
+    }
+    else {
+      window.alert("You can't afford that!");
+    }
+  }
+
+  return (
+    <main>
+      {score < 100 ? (
+        <>
+          <h1>Current Score: {score}</h1>
+          <button onClick={increaseScore}>+{pointIncrement}</button>
+          <button onClick={increasePointIncrement}>Pay 10 points to change from +{pointIncrement} to +{pointIncrement + 1}</button>
+        </>
+        ) : (
+        <>
+          <h2>You Win!</h2>
+          <button onClick={startOver}>Play again?</button>
+        </>
+      )}
+    </main>
+  );
 }
 
 export default App;
