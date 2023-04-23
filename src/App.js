5bba6983ec3506eb9037b8c56432c5809811aa56
Adrian Burke <114108495+AdrianBurke1@users.noreply.github.com>
2023-04-23 17:23:37 -0400

    lab-react-state-p rimitive-values9.6

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 import "./App.css";
+import { useState } from "react";
 
 function App () {
+  const [score, setScore] = useState(0)
+  const [increment, setIncrement] = useState(1)
+
+  function changeScore(){
+    setScore(score + increment)
+  }
+  
     return (
       <main>
         <p>React State Lab</p>
