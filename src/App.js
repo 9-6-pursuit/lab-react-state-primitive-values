a973f8bbf1ed65e1f351430e1790239e08aa3172
Justin Knolley <justinknolley@pursuit.org>
2023-04-24 18:29:24 -0400

    Lab

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from "react";
+import { useState } from "react";
 import "./App.css";
 
 function App () {
+    const [score, setScore] = useState(0)
+    const [pointIncrement, setPointIncrement] = useState(1)
+    
+    function inscreaseScore () {
+      setScore(score + pointIncrement)
+    }
+    
+    function increasePointIncrement () {
+      if (score >= 10) {
+        setScore(score - 10)
+        setPointIncrement(pointIncrement + 1)
+      } else {
+        window.alert("You can't afford that!")
+      }
+    }
+
     return (
       <main>
-        <p>React State Lab</p>
+        <h1>Current Score: {score}</h1>
+        {score < 100 ? (
+          <div>
+            <button onClick={inscreaseScore}>+{pointIncrement}</button>
+            <button onClick={increasePointIncrement}>Pay 10 points to change from +{pointIncrement + 1}</button>
+          </div>
+        )}
       </main>
-    );
+    )
 }
 
 export default App;
