eff95d2b9b6bb0068642d17ad47e96d84ac98969
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-04-24 03:30:26 -0400

    Finished react-state-primitive-values lab

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
-import React from "react";
+// import React, { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
-function App () {
-    return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
+function App() {
+  const [score, setScore] = useState(0);
+  const [pointIncrement, setPointIncrement] = useState(1);
+
+  function increaseScore() {
+    setScore(score + pointIncrement)
+  }
+
+  function increasePointIncrement() {
+    if (score >= 10) {
+      setScore(score - 10)
+      setPointIncrement(pointIncrement + 1)
+    } else {
+      window.alert("You can't afford that!")
+    }
+  }
+
+  const playAgain = () => {
+    setScore(0)
+    setPointIncrement(1)
+
+  };
+
+  return (
+    <main style={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      textAlign: "center",
+    }}>
+      {score < 100 ? (
+        <div>
+          <h1>Current Score: {score}</h1>
+          <div style={{ marginBottom: "10px" }}>
+            <button onClick={increaseScore}>+{pointIncrement}</button>
+          </div>
+          <div>
+            <button onClick={increasePointIncrement}>Pay 10 points to change from +{pointIncrement} to +{pointIncrement + 1}</button>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <h2>You Win!</h2>
+          <button onClick={playAgain}>Play again?</button>
+        </div>
+      )}
+    </main>
+  );
 }
 
 export default App;
