68ace29b051811e1397a27957eb97a94db3cb61d
Monir Hossen <monirhossen@pursuit.org>
2023-04-24 16:59:33 -0400

    state/promps lab

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React from "react";
 import "./App.css";
-
+import { useState } from "react";
 function App () {
+  const [score, setScore] =useState(0)
+  const [pointIncrement, setPointIncrement] =useState(1)
+  
+  function increaseScore(){
+    setScore(score + pointIncrement)
+  }
+
+  function increasePointIncrement(){
+    if(score >= 10){
+      setScore(score-10)
+      setPointIncrement(pointIncrement+1)
+    } else{
+      window.alert("You can not afford that!")
+    }
+  }
+
     return (
       <main>
-        <p>React State Lab</p>
+        <p>Current Score: {score}</p>
+        {score <100 ? (
+          <div>
+            <button onClick = {increaseScore}> + {pointIncrement} </button>
+            <button onClick={increasePointIncrement}>Pay 10 points to change from {pointIncrement} </button>
+          </div>
+        ):(
+          <h2> You win!</h2>
+        )}
       </main>
     );
 }
