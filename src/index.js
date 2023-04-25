e5f77a43522dfcaa155eb726f4915200e5e488b3
AlejandraRV91 <alejandraramirez@pursuit.org>
2023-04-25 00:20:42 -0500

    lab react primitive values DONE

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+/** @format */
+
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+	<React.StrictMode>
+		<App score={0} adition={1} />
+	</React.StrictMode>,
+	document.getElementById("root")
 );
