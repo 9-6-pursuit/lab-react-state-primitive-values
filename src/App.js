da567d3d5eded17d2607b37144f3b6a68ad0cda0
LELIAH <LELIAH791@GMAIL.COM>
2023-04-24 21:47:09 -0400

    lab completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import React from "react";
 import "./App.css";
+import { useState } from "react";
 
 function App () {
+  const [score, setScore] = useState(0);
+  const [points, setPoints] = useState(1)
+  const [scoreDisplay, setScoreDisplay] = useState(true)
+
+  function addPoint(){
+    setScore(score + points)
+  }
+
+  function payPoints(){
+    if(score >= 10) {
+      setPoints(points +1)
+      setScore(score - 10)
+    } else {
+      alert(`You can't afford that!`)
+    }
+
+    //CAN WE WRITE A TERNARY W/ MULTI EXPRESSIONS???
+    // score > 10 ? (setPoints(points + 1) && setScore(score - 10)) : alert('not enough')
+  }
+
+  function playAgain(){
+    setScore(0)
+    setPoints(1)
+  }
+
     return (
       <main>
-        <p>React State Lab Teest</p>
+        {(score < 100) ? (
+          <div>
+             <h3 style={scoreDisplay ? {display: 'block'} : {display: 'none'}}>Current Score: {score}</h3>
+             <button onClick={addPoint}>+{points}</button> <br/>
+             <button onClick={payPoints}>Pay 10 points to change from +{points} to +{points + 1 } </button>
+          </div>
+        ) : (
+          <div>
+            <h2>You Win!</h2>
+            <button onClick={playAgain}> Play again?</button>
+          </div>
+        ) }
       </main>
     );
 }
