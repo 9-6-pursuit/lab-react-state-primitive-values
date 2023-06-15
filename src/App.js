972f4d776bd8f153ad0760efd4f10190b15c556e
winterbelle <blancaaltamirano@pursuit.org>
2023-06-15 01:41:13 -0400

    lab completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React from "react";
+import { useState } from "react";
 import "./App.css";
 
 function App () {
+  const [score, setScore] = useState(0);
+  const [payAmount, setPayAmount] = useState(1);
+
+  function increaseScore() {
+    setScore(score + payAmount)
+  }
+
+  function increasePayAmount(){
+    if (score >= 10) {
+      setScore(score - 10)
+      setPayAmount(payAmount + 1)
+    }
+    window.alert("You can't afford that!")
+  }
+
+
     return (
       <main>
-        <p>React State Lab</p>
+        {score < 100 ? (
+          <div>
+            <h1>Current Score: {score}</h1>
+            <button onClick={increaseScore}>+{payAmount}</button>
+            <br></br>
+            <button onClick={increasePayAmount}>Pay 10 points to change from +{payAmount} to +{payAmount + 1}</button>
+          </div>
+        ) : (
+          <>
+          <h2>You Win !</h2>
+          <button onClick={() => window.location.reload(false)}>Play Again ?</button>
+          </>
+        )}
       </main>
     );
 }
